036b82cbb77d8082745d854796c6961dc559f443
LeonardoRHernandez <leonardoroque711@gmail.com>
2024-01-20 17:14:30 -0600

    modificando informacion usuario

--- src/build/controllers/usuariosControllers.js
+++ src/build/controllers/usuariosControllers.js
@@ -55,7 +55,7 @@ class UsuariosController {
     iniciarSesionU(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
             const parametros = req.body;
-            var consulta = `SELECT correo FROM usuarios WHERE correo = '${parametros.correo}' and contrasenia = '${parametros.contrasenia}'`;
+            var consulta = `SELECT nombre telefono correo FROM usuarios WHERE correo = '${parametros.correo}' and contrasenia = '${parametros.contrasenia}'`;
             const resp = yield database_1.default.query(consulta);
             if (resp.length > 0) {
                 res.json(resp);
